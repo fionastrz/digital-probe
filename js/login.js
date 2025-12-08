b10b2fc7b571303cbf8ea903a312bb1ed9e3983e
fionastrz <115540296+fionastrz@users.noreply.github.com>
2025-12-08 16:24:15 +0100

    login + registrierung

---
+++ js/login.js
@@ -0,0 +1,132 @@
+const supabaseUrl = "https://hrswrvqavtkqcrvprrsm.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhyc3dydnFhdnRrcWNydnBycnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMTEyOTksImV4cCI6MjA4MDY4NzI5OX0.pTWsDXH8c4MQZE7KoJ1JEyOk0XY_FP5KdMOgzmdDftk";
+
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+function switchToRegister() {
+  const container = document.getElementById("form-container");
+  container.innerHTML = `
+<h2>Erstelle einen neuen Account</h2>
+<form id="registerForm">
+<label for="reg-email">E-Mail</label>
+<input type="email" id="reg-email" placeholder="E-Mail eingeben" required />
+
+
+<label for="reg-password">Passwort</label>
+<input type="password" id="reg-password" placeholder="Passwort erstellen" required />
+
+<label for="reg-password2">Passwort wiederholen</label>
+<input type="password" id="reg-password2" placeholder="Passwort wiederholen" required />
+
+<button type="submit">Registrieren</button>
+</form>
+
+<div id="message"></div>
+
+<div class="switch">
+Du hast bereits einen Account? <a href="#" onclick="switchToLogin()">Zum Login</a>
+</div>
+`;
+  attachRegisterHandler(); // Registrierung aktivieren
+}
+
+function switchToLogin() {
+  const container = document.getElementById("form-container");
+  container.innerHTML = `
+<h2>Login</h2>
+<form id="loginForm">
+<label for="login-email">E-Mail</label>
+<input type="email" id="login-email" placeholder="E-Mail eingeben" required />
+
+
+<label for="login-password">Passwort</label>
+<input type="password" id="login-password" placeholder="Passwort eingeben" required />
+
+
+<button type="submit">Anmelden</button>
+
+
+<div class="link">
+<a href="#">Passwort vergessen?</a>
+</div>
+</form>
+<div class="switch">
+Du hast noch keinen Account? <a href="#" onclick="switchToRegister()">Jetzt registrieren</a>
+</div>
+`;
+  attachLoginHandler(); // Login aktivieren
+}
+
+function attachRegisterHandler() {
+  const form = document.getElementById("registerForm");
+  const msg = document.getElementById("message");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    msg.textContent = "";
+
+    const email = document.getElementById("reg-email").value;
+    const password = document.getElementById("reg-password");
+    const password2 = document.getElementById("reg-password2");
+
+    if (password.value !== password2.value) {
+      showToast("Die Passwörter stimmen nicht überein.", "error");
+      password.style.borderColor = "red";
+      password2.style.borderColor = "red";
+      return;
+    }
+
+    const { data, error } = await supabaseClient.auth.signUp({
+      email: email,
+      password: password.value,
+    });
+
+    if (error) {
+      showToast("Fehler bei der Registrierung.", "error");
+      console.error("Sign-Up Error:", error.message);
+    } else {
+      showToast("Registrierung erfolgreich.", "success");
+      switchToLogin();
+    }
+  });
+}
+
+function attachLoginHandler() {
+  const form = document.getElementById("loginForm");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      showToast(error.message, "error");
+      console.error("Login Error:", error.message);
+    } else {
+      console.log("User:", data.user);
+      window.location.href = "home.html";
+    }
+  });
+}
+
+function showToast(message, type) {
+  const container = document.getElementById("toast-container");
+
+  const toast = document.createElement("div");
+  toast.classList.add("toast", type);
+  toast.textContent = message;
+
+  container.appendChild(toast);
+
+  // Toast nach 3 Sekunden entfernen
+  setTimeout(() => {
+    toast.remove();
+  }, 3000);
+}
+
+switchToLogin();
